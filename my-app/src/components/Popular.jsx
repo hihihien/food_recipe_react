fd43db8b300b0facad08f7669b6ee3e934200d8b
Hien Giang <gianghien2999@gmail.com>
2022-06-03 13:02:11 +0200

    changed

--- my-app/src/components/Popular.jsx
+++ my-app/src/components/Popular.jsx
@@ -1,5 +1,13 @@
 
 function Popular() {
-  return <div>Popular</div>}
+
+    const getPopular = async () => {
+        const api = await fetch(`https://spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+        const data = await api.json();
+        console.log(data);
+    };
+
+  return <div>Popular</div>;
+}
 
 export default Popular;
