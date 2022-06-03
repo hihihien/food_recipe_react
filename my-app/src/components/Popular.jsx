c3ae59cb9e5d6d1aed3ae4013e832ce197d1d898
Hien Giang <gianghien2999@gmail.com>
2022-06-03 14:21:24 +0200

    error

--- my-app/src/components/Popular.jsx
+++ my-app/src/components/Popular.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react"; 
 
 function Popular() {
+    
+    useEffect(() => {
+        getPopular();
+    },[]);
 
     const getPopular = async () => {
-        const api = await fetch(`https://spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+        const api = await fetch(`https://spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`); //number of items you wanna get back
         const data = await api.json();
         console.log(data);
     };
