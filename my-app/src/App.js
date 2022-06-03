5dae6620083e0c801cb8d5c188f6623da1cc81d8
Hien Giang <gianghien2999@gmail.com>
2022-06-03 11:05:48 +0200

    create components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
+import Home from "../pages/Home";
 
 function App() {
   return (
     <div className="App">
      
-    <h1>Hello</h1>
+      <h1>Hello</h1>
+      <Home />
+    <nav>
+    </nav>
     </div>
   );
 }
