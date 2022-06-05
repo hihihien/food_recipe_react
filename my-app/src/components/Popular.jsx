6d1d3841cc96e73c9cfc30faa05d3abccf95d4be
Hien Giang <gianghien2999@gmail.com>
2022-06-05 16:22:56 +0200

    changed

--- my-app/src/components/Popular.jsx
+++ my-app/src/components/Popular.jsx
@@ -5,9 +5,9 @@ function Popular() {
     useEffect(() => {
         getPopular();
     },[]);
-
+    
     const getPopular = async () => {
-        const api = await fetch(`https://spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`); //number of items you wanna get back
+        const api = await fetch(`https://spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`); 
         const data = await api.json();
         console.log(data);
     };
