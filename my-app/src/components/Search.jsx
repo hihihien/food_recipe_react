965a9487e18db47f48f857bb6447a090e4b2d164
Hien Giang <gianghien2999@gmail.com>
2022-06-09 20:09:33 +0200

    edit search function

--- my-app/src/components/Search.jsx
+++ my-app/src/components/Search.jsx
@@ -1,21 +1,42 @@
-import styled from 'styled-components';
-import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
-
-import React from 'react'
+import styled from "styled-components";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
+
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/" + input);
+  };
+
+
   return (
-    <FormStyle>
-        <input type="text"/>
+    <FormStyle onSubmit={submitHandler}>
+      <div>
+        <FaSearch></FaSearch>
+        <input 
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+        />
+      </div>
+      <h1>{input}</h1>
     </FormStyle>
-  )
+  );
 }
 
 const FormStyle = styled.form`
  margin: 0rem 20rem;
- position: relative;
- width: 100%;
+
+  div{
+    width: 100%;
+    position: relative;
+  }
+ 
  input{
     border: none;
     background: linear-gradient(35deg, #494949, #313131);
@@ -25,6 +46,7 @@ const FormStyle = styled.form`
     border: none;
     border-radius: 1rem;
     outline: none;
+    width: 100%;
  }
 
  svg{
@@ -35,5 +57,5 @@ const FormStyle = styled.form`
     color: white;
 
  }
-`
+`;
 export default Search
---
+++ my-app/src/pages/Searched.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Searched() {
+
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
+    let params = useParams();
+  
+    const getSearched = async (name) => {
+        const data = await fetch(
+            `https://api.spoonacular.com/recipes/complexSearch?apiKey=29094c27cad640a791a7d50d78b5de3e&query=${name}`
+        )
+        const recipes = await data.json();
+        setSearchedRecipes(recipes.results);
+    };
+  
+    useEffect(() => {
+        getSearched(params.search);
+    },[params.search]);
+
+    return (
+    <Grid>
+        {searchedRecipes.map((iten) => {
+            return(
+                <Card key={item.id}>
+                    <img src={item.image} alt="" />
+                    <h4>{item.title}</h4>
+                </Card>
+            )
+        })}
+    </Grid>
+  )
+}
+
+const Grid = styled.div `
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-grap: 3rem;
+`;
+
+const Card = styled.div `
+    img{
+        innerWidth: 100%;
+        border-radius; 2rem;
+    }
+    a{
+        text-decoration: FaBorderNone;
+    }
+    h4{
+        text-align: FaCentercode;
+        padding: 1rem;
+    }
+`;
+
+export default Searched
